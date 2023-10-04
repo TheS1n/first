c0c023c20839492569b9725d43d4376c8e184604
TheS1n <96494091+TheS1n@users.noreply.github.com>
2023-10-04 09:59:33 +0300

    Add files via upload

---
+++ Evclid/js/script.js
@@ -0,0 +1,55 @@
+/* slider */
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+});
+
+/* work-tabs */
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.work__btn').forEach(function (e) {
+    e.addEventListener('click', function (e) {
+      const tab = e.currentTarget.dataset.path;
+      document.querySelectorAll('.tab-content').forEach(function (e) {
+        e.classList.remove('tab-content--active')
+        document.querySelector(`[data-target='${tab}']`).classList.add('tab-content--active');
+      });
+    });
+
+    e.addEventListener('click', function (e) {
+      const tabDefault = e.currentTarget.dataset.default;
+      document.querySelectorAll('.work__btn').forEach(function (e) {
+        e.classList.remove('work__btn--default')
+        document.querySelector(`[data-path='${tabDefault}']`).classList.add('work__btn--default');
+      });
+    });
+
+  });
+})
+
+// accordion
+document.addEventListener("DOMContentLoaded", function() {
+  let acc = new Accordion('.accordion__list', {
+  duration: 700,
+  elementClass: 'accordion__item',
+  triggerClass: 'accordion__top',
+  panelClass: 'accordion__bottom',
+
+});
+})
+
+
+//bruger
+const burgerBtn = document.querySelector('.burger');
+const menuClose = document.querySelector('.menu-close');
+const menuBurger = document.querySelector('.nav');
+
+burgerBtn.addEventListener('click', () => {
+  menuBurger.classList.add('burger-active');
+});
+
+menuClose.addEventListener('click', () => {
+  menuBurger.classList.remove('burger-active');
+});
+
